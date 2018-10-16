2aafeaf1efe21287cff78fec661f15df9b7fb66c
unknown <brad_gemini@hotmail.com>
2018-10-16 16:25:01 +0800

    oct16th

---
+++ complete-javascript-course-master/5-advanced-JS/starter/script.js
@@ -0,0 +1,77 @@
+/*
+var john ={
+    name:'John',
+    yearOfBrith:1990,
+    job:'teacher'  
+};
+
+var Person = function(name, yearOfBirth,job){
+    this.name = name;
+    this.yearOfBrith = yearOfBirth;
+    this.job = job;  
+}
+
+Person.prototype.calcAge = function(){
+     console.log(2016 - this.yearOfBrith);
+}
+
+var john = new Person('John', 1990,'teacher');
+
+john.calcAge();
+
+var jane = new Person('Jane',1969,'designer');
+var mark = new Person('Mark',1948,'retired');
+
+var personProto = {
+    calcAge :function(){
+        console.log(2016-this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'john';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane= Object.create(personProto,{
+    name:{value:'jane'},
+    yearOfBirth : {value: 1990},
+    job: { value:'designer'}
+});
+
+
+var years = [1990, 1965, 1938 ,2005, 1998];
+
+function arrayCalc(arr, fn){
+    var  arrRes = [];
+    for(var i = 0; i< arr.length;i++ ){
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calcAge(el){
+    return 2016 - el;
+}
+
+function isFullAge(el){
+    return el >= 18;
+}
+
+function maxHeartRate(el){
+    if(el >= 18 && el <= 81){
+       return Math.round(206.9-(0.67*el)); 
+    }else{
+        return -1;
+    }
+}
+
+var ages = arrayCalc(years,calcAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages,maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
+
+*/
