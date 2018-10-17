8aeee6b3672632b8e14759c5f8c1d6106d894977
unknown <brad_gemini@hotmail.com>
2018-10-17 15:20:28 +0800

    oct17th

--- complete-javascript-course-master/5-advanced-JS/starter/script.js
+++ complete-javascript-course-master/5-advanced-JS/starter/script.js
@@ -75,3 +75,14 @@ console.log(fullAges);
 console.log(rates);
 
 */
+
+function retirement(retirementAge){
+    var a =' years left until retirement.';
+    return function(yearOfBirth){
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge-age) +a); 
+    }
+}
+
+var retirementUS = retirement(66);
+retirementUS(1990);
